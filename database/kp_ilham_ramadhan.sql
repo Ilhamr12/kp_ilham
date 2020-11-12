a5e9caeb388e40f346a40c33c3f227d5f645b132
Ilhamr12 <ilham.r0199@gmail.com>
2020-11-12 17:13:59 +0700

    update database

--- database/kp_ilham_ramadhan.sql
+++ database/kp_ilham_ramadhan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 28, 2020 at 10:30 AM
+-- Generation Time: Nov 12, 2020 at 11:11 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
@@ -34,6 +34,14 @@ CREATE TABLE `tb_absen` (
   `waktu` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `tb_absen`
+--
+
+INSERT INTO `tb_absen` (`id`, `id_karyawan`, `nama`, `waktu`) VALUES
+(1, 202010, 'Ilham Ramadhan', 'Thursday, 12-11-2020 01:43:00 pm'),
+(2, 202010, 'Ilham Ramadhan', 'Thursday, 12-11-2020 03:51:27 pm');
+
 -- --------------------------------------------------------
 
 --
@@ -46,6 +54,13 @@ CREATE TABLE `tb_daftar` (
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `tb_daftar`
+--
+
+INSERT INTO `tb_daftar` (`id`, `username`, `password`) VALUES
+(1, 'ilham', 'ramadhan');
+
 -- --------------------------------------------------------
 
 --
@@ -57,6 +72,17 @@ CREATE TABLE `tb_jabatan` (
   `jabatan` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `tb_jabatan`
+--
+
+INSERT INTO `tb_jabatan` (`id`, `jabatan`) VALUES
+(1, 'CEO'),
+(2, 'CTO'),
+(3, 'CFO'),
+(4, 'WP Pemasaran'),
+(5, 'Office Boy');
+
 -- --------------------------------------------------------
 
 --
@@ -64,7 +90,7 @@ CREATE TABLE `tb_jabatan` (
 --
 
 CREATE TABLE `tb_karyawan` (
-  `id` int(11) NOT NULL,
+  `id_karyawan` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `nama` varchar(255) NOT NULL,
@@ -76,6 +102,16 @@ CREATE TABLE `tb_karyawan` (
   `jabatan` varchar(255) NOT NULL,
   `foto` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_karyawan`
+--
+
+INSERT INTO `tb_karyawan` (`id_karyawan`, `username`, `password`, `nama`, `tmp_tgl_lahir`, `jenis_kelamin`, `agama`, `alamat`, `no_tlp`, `jabatan`, `foto`) VALUES
+(202010, 'ilham', '57cf5ad49695e3adc1a29cf47a43bc06', 'Ilham Ramadhan', 'Jakarta, 01 Januari 1999', 'Laki-laki', 'Islam', 'Kp. Rawa badung, RT.08/RW.07, Kec. Cakung, Kel. Jatinegara', '081213369914', 'CEO', '1211202007372814102020110519timthumb.png'),
+(202011, 'sulthan', 'e7ddef6c0917ed2642494bc4904d2e4c', 'Sulthan Alawy Shihab', 'Jakarta, 30 Januari 1998', 'Laki-laki', 'Islam', 'Parung Bogor', '089698767819', 'CEO', '1211202007391514102020110519timthumb.png'),
+(202012, 'muslih', '82c8b45ed8a25cf38b41e88fb0d22078', 'Muslih Badruttamam', 'Bekasi, 25 Desember 1999', 'Laki-laki', 'Islam', 'Jl. Panda', '089878902134', 'CFO', '12112020074228bitcoin-2008262_1920.jpg'),
+(202013, 'salman', '97502267ac1b12468f69c14dd70196e9', 'Ahmad Salman Farisi', 'Jakarta', 'Laki-laki', 'Islam', 'Tebet', '089878657432', 'CEO', '12112020111111perusahaan.png.jpg');
 
 -- --------------------------------------------------------
 
@@ -92,6 +128,15 @@ CREATE TABLE `tb_keterangan` (
   `waktu` varchar(255) NOT NULL,
   `bukti` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_keterangan`
+--
+
+INSERT INTO `tb_keterangan` (`id`, `id_karyawan`, `nama`, `keterangan`, `alasan`, `waktu`, `bukti`) VALUES
+(4, 202010, 'Ilham Ramadhan', 'Izin', 'sakit', 'Thursday, 12-11-2020 03:53:09 pm', '1211202009532614102020110519timthumb.png'),
+(5, 202010, 'Ilham Ramadhan', 'Izin', 'sakit', 'Thursday, 12-11-2020 04:07:55 pm', '1211202010083514102020110519timthumb.png'),
+(6, 202010, 'Ilham Ramadhan', 'Sakit', 'aaaaaaaaa', 'Thursday, 12-11-2020 04:16:29 pm', '1211202010165614102020110519timthumb.png');
 
 --
 -- Indexes for dumped tables
@@ -120,7 +165,7 @@ ALTER TABLE `tb_jabatan`
 -- Indexes for table `tb_karyawan`
 --
 ALTER TABLE `tb_karyawan`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_karyawan`);
 
 --
 -- Indexes for table `tb_keterangan`
@@ -137,31 +182,31 @@ ALTER TABLE `tb_keterangan`
 -- AUTO_INCREMENT for table `tb_absen`
 --
 ALTER TABLE `tb_absen`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tb_daftar`
 --
 ALTER TABLE `tb_daftar`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tb_jabatan`
 --
 ALTER TABLE `tb_jabatan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tb_karyawan`
 --
 ALTER TABLE `tb_karyawan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_karyawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15468029;
 
 --
 -- AUTO_INCREMENT for table `tb_keterangan`
 --
 ALTER TABLE `tb_keterangan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
@@ -171,13 +216,13 @@ ALTER TABLE `tb_keterangan`
 -- Constraints for table `tb_absen`
 --
 ALTER TABLE `tb_absen`
-  ADD CONSTRAINT `tb_absen_ibfk_1` FOREIGN KEY (`id_karyawan`) REFERENCES `tb_karyawan` (`id`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `tb_absen_ibfk_1` FOREIGN KEY (`id_karyawan`) REFERENCES `tb_karyawan` (`id_karyawan`);
 
 --
 -- Constraints for table `tb_keterangan`
 --
 ALTER TABLE `tb_keterangan`
-  ADD CONSTRAINT `tb_keterangan_ibfk_1` FOREIGN KEY (`id_karyawan`) REFERENCES `tb_absen` (`id`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `tb_keterangan_ibfk_1` FOREIGN KEY (`id_karyawan`) REFERENCES `tb_karyawan` (`id_karyawan`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
